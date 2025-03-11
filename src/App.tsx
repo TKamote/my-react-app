fd26892a44dbe5457a639f4a4d5b465c1d5566bb
KamoteNga <kamoteka143din@gmail.com>
2025-03-11 23:57:19 +0800

    3rd commit

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <div>
-        <h1>Dave</h1>
+        <h1>Fucking Dave is awesome</h1>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
